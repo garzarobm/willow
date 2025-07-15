e52b9b989fd5a76ceed01b956d5c2414b11e197b
Mike <garzarobm@gmail.com>
2025-07-15 05:09:46 -0500

    Update .gitignore to exclude Articles template and add 'Add Note' link in Contacts index

--- plugins/ContactManager/templates/Contacts/index.php
+++ plugins/ContactManager/templates/Contacts/index.php
@@ -34,6 +34,7 @@
             <td><?= h($contact->country) ?></td>
             <td class="actions">
                 <?= $this->Html->link(__('View'), ['action' => 'view', $contact->id]) ?>
+                <?= $this->Html->link(__('Add Note'), ['action' => 'addNote', $contact->id]) ?>
                 <?= $this->Html->link(__('Edit'), ['action' => 'edit', $contact->id]) ?>
                 <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id)]) ?>
             </td>
